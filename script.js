721ff11a185b09ed1699c84ab0fd2d5de13d8485
FrancoisDemilly <francois.demilly@gmail.com>
2018-12-19 18:00:50 -0500

    desktop version

--- script.js
+++ script.js
@@ -25,16 +25,16 @@ var initModal = function(){
   let faSimon = document.getElementById('fa-simon'); //for opennig the modal window
   let faTictactoe = document.getElementById('fa-tictactoe'); //for opennig the modal window
 
-  var pomodoro = document.getElementById('pomodoro');
-  var calculator = document.getElementById('calculator');
-  var tictactoe = document.getElementById('tictactoe');
-  var simon = document.getElementById('simon');
+  let pomodoro = document.getElementById('pomodoro');
+  let calculator = document.getElementById('calculator');
+  let tictactoe = document.getElementById('tictactoe');
+  let simon = document.getElementById('simon');
 
-  var nextModal = document.getElementById('next-modal');
-  var prevModal = document.getElementById('prev-modal');
+  let nextModal = document.getElementById('next-modal');
+  let prevModal = document.getElementById('prev-modal');
 
   nextModal.addEventListener('click', function(){
-    var margin = window.getComputedStyle(containerSlider).
+    let margin = window.getComputedStyle(containerSlider).
     getPropertyValue('margin-left');
 
     switch(margin){
@@ -54,7 +54,7 @@ var initModal = function(){
   }, false)
 
   prevModal.addEventListener('click', function(){
-    var margin = window.getComputedStyle(containerSlider).
+    let margin = window.getComputedStyle(containerSlider).
     getPropertyValue('margin-left');
 
     switch(margin){
@@ -84,32 +84,63 @@ var initModal = function(){
   //openning the modal window for each project
 
   faPomodoro.addEventListener('click', function(){
-    containerSlider.style.marginLeft = '0px'
-    modalDisplay.style.visibility = 'visible'
+    containerSlider.style.marginLeft = '0px';
+    modalDisplay.style.visibility = 'visible';
     containerSlider.style.visibility = 'visible';
     navModal.style.visibility = 'visible';
   })
 
   faCalculator.addEventListener('click', function(){
     containerSlider.style.marginLeft = '-650px';
-    modalDisplay.style.visibility = 'visible'
+    modalDisplay.style.visibility = 'visible';
     containerSlider.style.visibility = 'visible';
     navModal.style.visibility = 'visible';
   })
 
   faSimon.addEventListener('click', function(){
-    containerSlider.style.marginLeft = '-1300px'
-    modalDisplay.style.visibility = 'visible'
+    containerSlider.style.marginLeft = '-1300px';
+    modalDisplay.style.visibility = 'visible';
     containerSlider.style.visibility = 'visible';
     navModal.style.visibility = 'visible';
   })
 
   faTictactoe.addEventListener('click', function(){
-    containerSlider.style.marginLeft = '-1950px'
-    modalDisplay.style.visibility = 'visible'
+    containerSlider.style.marginLeft = '-1950px';
+    modalDisplay.style.visibility = 'visible';
     containerSlider.style.visibility = 'visible';
     navModal.style.visibility = 'visible';
   })
+
+
+  //sliding slidebar
+      let sidebar = document.getElementById('sidebar');
+      let toggle = document.getElementById('toggle-sidebar')
+
+      toggle.addEventListener('click', function(){
+        console.log('toggle', toggle);
+        let webkitAnimationName =
+          window.getComputedStyle(sidebar).getPropertyValue('-webkit-animation-name');
+        let animationName =
+          window.getComputedStyle(sidebar).getPropertyValue('animation-name');
+        let marginLeft =
+          window.getComputedStyle(sidebar).getPropertyValue('margin-left');
+
+        if(webkitAnimationName != ''){
+          ((webkitAnimationName != 'open') ?
+            (sidebar.style.webkitAnimationName = 'open', sidebar.style.marginLeft = '0px') :
+            (sidebar.style.webkitAnimationName = 'close' , sidebar.style.marginLeft = '-180px'))
+          console.log('from webkitAnimationName',marginLeft);
+        }
+
+        if(animationName != null){
+          ((animationName != 'open') ?
+            (sidebar.style.animationName = 'open', sidebar.style.marginLeft = '0px') :
+            (sidebar.style.animationName = 'close', sidebar.style.marginLeft = '-180px'))
+          console.log('from animationName', marginLeft);
+        }
+        console.log('after', marginLeft);
+      }, false)
+
 
 }
 
